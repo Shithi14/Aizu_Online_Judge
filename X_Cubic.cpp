cad5b72e72607e170b0e72c363104e4dceddc10f
Shithi Rani Roy <cse12105009brur@gmail.com>
2025-03-16 23:44:48 +0600

    Add files via upload

---
+++ X_Cubic.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<n*n*n<<endl;
+    
+   
+
+return 0;
+}
